199f9ac6168e347613ce8a759569b178cac740c8
Dimas <dima18127@yandex.ru>
2022-10-19 16:56:13 +0400

    first commit, before store

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,134 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap/dist/css/bootstrap.min.css" />
+<!-- <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css" /> -->
+  
+<div id ="app" class="appe">
+  <count-comp></count-comp>
+      <table class="OurTable">
+        <tr>
+          <th v-for="item, index in MainString" :key=index > {{item }}    </th>
+        </tr>
+        <tr  v-for="product, index of LoadMorePages" :key="index">
+          <td > {{generateRandomDate()}} </td>
+          <td> {{product.title}} </td>
+          <td > {{product.stock}} </td>
+          <td >  {{product.price}}</td>
+        </tr>
+        <div ></div>
+        <tr >
+        </tr>
+        <div v-if="Errored" class="alert alert-danger OurTable" role="alert">
+          Ошибка в запросе к серверу API
+        </div>
+      </table>
+          <container  class="pages">
+            <div v-on:click="LoadMorePages" class="page" v-for="page, index in Pages" :key="index">
+              {{page}}
+            </div>
+
+          </container>
+
+          <div v-for="prod, index in info" :key="index" >
+            {{prod}}
+          </div>
+      
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import axios from "axios"
+import  CountComp  from "./components/CountComp.vue";
 export default {
   name: 'App',
+  data(){
+    return {
+      info: [],
+      isActive: true,
+      MainString: ['Дата ', 'Название ', 'Колличество', 'Расстояние'],
+      Errored: false,
+      CurrentPage: 1,
+    }
+  },
   components: {
-    HelloWorld
+    CountComp
+  },
+  methods: {
+    generateRandomDate() { 
+    return Math.floor((Math.random()*10)+1) + ' centember 2022'; 
+    },
+    LoadMorePages(){
+      
+      let from = (this.CurrentPage - 1)*5
+      let to = from + 5
+      this.CurrentPage += 1
+      console.log(typeof(this.info.slice(from, to)));
+      this.info.slice(from, to)
+      console.log(this.info);
+    }
+  },
+
+  mounted() {
+    axios
+      .get('https://dummyjson.com/products?_limit=13')
+      .then(response => (this.info = response.data.products))
+      .catch(() => {
+        this.Errored = true
+      });
+  },
+  // кол-во страниц
+  computed: {
+    Pages(){
+      return Math.ceil(this.info.length/5)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.appe {
+  display: flex;
+  flex-direction:column;
+  align-items:center;
 }
+.OurTable {
+  display: block;
+  /* table-align: center; */
+  margin: 60px auto 10px;
+  font-size: 100px 30px;
+  color:rgb(0, 68, 45);
+  background-color: rgb(221, 221, 221);
+  border: 1px solid grey;
+  border-collapse: collapse;
+  padding-top: 10px;
+}
+th {
+  padding: 3px;
+}
+ tr, th, td {
+  /* color: rgb(231, 0, 0); */
+  border: 1px solid rgb(3, 3, 3)
+}
+#app > div > div{
+  display: block;
+  width: 50%;
+  margin: auto;
+}
+.pages {
+  display: flex;
+  
+}
+.page {
+  display: block;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  color: rgb(0, 68, 45);
+  border: 2px solid rgb(0, 0, 0);
+  margin-left: 10px;
+  cursor: pointer;
+}
+
 </style>
---
+++ src/components/CountComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Counter</h1>
+    <h2>{{$store.state.count}}</h2>
+    <button @click="increase" >НАЖМИ НА МЕНЯ</button>
+</template>
+<script>
+export default {
+    methods: {
+        increase(){
+            console.log(this);
+            this.$store.state.count++
+        }
+    }
+}
+</script>
+<style>
+</style>
